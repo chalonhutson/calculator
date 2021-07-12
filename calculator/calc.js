2c03f12dba7cb452b6adf011a1849c77e91b620a
chalonhutson <chalonhutson@gmail.com>
2021-07-12 14:29:36 -0600

    Some more edits.

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,9 +1,12 @@
+const { read } = require('fs');
 const readline = require('readline');
 
 const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+var runProgram = true;
 
 
 
@@ -13,23 +16,31 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	num3 = Number(tokens[3]);
 
+	if (mathSymbol === "end"){
+		console.log("Thanks!");
+		runProgram = false;
+	}
 	if (mathSymbol === "+"){
-		console.log(num1 + num2);
+		console.log(num1 + num2 + num3);
 	}
 	else if (mathSymbol === "-"){
-		console.log(num1 - num2);
+		console.log(num1 - num2 - num3);
 	}
 	else if (mathSymbol === "*"){
-		console.log(num1 * num2);
+		console.log(num1 * num2 * num3);
 	}
 	else if (mathSymbol === "/"){
-		console.log(num1 / num2);
+		console.log(num1 / num2 / num3);
 	}
 	else if (mathSymbol === "**"){
 		console.log(num1 ** num2);
 	}
-	else if (mathSymbol === "√"){
+	else if (mathSymbol === "***"){
+		console.log(num1 ** 3);
+	}
+	else if ((mathSymbol === "sqrt") || (mathSymbol === "√")){
 		if (num2 == Number){
 			console.log(Math.sqrt(num1));
 			console.log(Math.sqrt(num2));
@@ -43,13 +54,15 @@ reader.question("What would you like to calculate?", function(input){
 	}
 
 
-	// console.log('mathSymbol - ', mathSymbol);
-	// console.log("number 1 - ", +num1);
-	// console.log("number 2 - ", +num2);
+	//console.log('mathSymbol - ', mathSymbol);
+	//console.log("number 1 - ", num1);
+	//console.log("number 2 - ", num2);
 	
 
 	// This line closes the connection to the command line interface.
-	reader.close()
+
 
 });
+
+
 
