0188e1ec58ea918d037b1a1782643a53431efd17
chalonhutson <chalonhutson@gmail.com>
2021-07-12 13:18:38 -0600

    Second push.

--- calculator/calc.js
+++ calculator/calc.js
@@ -14,8 +14,42 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+	}
+	else if (mathSymbol === "-"){
+		console.log(num1 - num2);
+	}
+	else if (mathSymbol === "*"){
+		console.log(num1 * num2);
+	}
+	else if (mathSymbol === "/"){
+		console.log(num1 / num2);
+	}
+	else if (mathSymbol === "**"){
+		console.log(num1 ** num2);
+	}
+	else if (mathSymbol === "√"){
+		if (num2 == Number){
+			console.log(Math.sqrt(num1));
+			console.log(Math.sqrt(num2));
+		}
+		else {
+			console.log(Math.sqrt(num1));
+		}
+	}
+	else {
+		console.log("Type in the correct thing, jerk!")
+	}
+
+
+	console.log('mathSymbol - ', mathSymbol);
+	console.log("number 1 - ", +num1);
+	console.log("number 2 - ", +num2);
+	
 
 	// This line closes the connection to the command line interface.
 	reader.close()
 
 });
+
